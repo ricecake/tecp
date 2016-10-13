2d26a0cd27c6faa684c541bfa984afddf698686e
Sebastian Green-Husted <ricecake@tfm.nu>
2016-10-13 19:25:24 -0400

    make string configurable

--- src/stream.c
+++ src/stream.c
@@ -1,26 +1,27 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "scmo.h"
 #include "util.h"
 
-int main() {
+int main(int argc, char* argv[]) {
 	//FILE* fp = fopen("/tmp/test.txt", "w+");
 
-	const char* string = "This is an example string";
+	uint8_t* buffer = (uint8_t*)malloc(strlen(argv[1]) + 1);
 
-	puts(string);
+	puts(argv[1]);
 
-	uint8_t* buffer = (uint8_t*)malloc(strlen(string) + 1);
-	scmo_cipher((uint8_t*)string, buffer, strlen(string) + 1, 0xdeadbeefcafebabe);
+	scmo_cipher((uint8_t*)argv[1], buffer, strlen(argv[1]) + 1, 0xdeadbeefcafebabe);
 
-	for(uint8_t i = 0; i < 26; i++) {
+	for(uint8_t i = 0; i < strlen(argv[1]) + 1; i++) {
 		printf("%02X", buffer[i]);
 	}
 
 	puts("");
 
-	scmo_cipher(buffer, buffer, strlen(string) + 1, 0xdeadbeefcafebabe);
+	scmo_cipher(buffer, buffer, strlen(argv[1]) + 1, 0xdeadbeefcafebabe);
 	puts((char*)buffer);
 }
