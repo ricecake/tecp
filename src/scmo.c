653e4363a2e54d2f3c1a5f93112d8a02e41fd736
Sebastian Green-Husted <ricecake@tfm.nu>
2016-10-13 18:40:46 -0400

    formatting

--- src/scmo.c
+++ src/scmo.c
@@ -8,9 +8,11 @@ uint8_t* scmo_encrypt(uint8_t* data, uint8_t* buffer, size_t data_size, scmo_key
 	for(size_t i = 0; i < data_size; i++) {
 		uint8_t j = i % 4;
 		uint8_t c;
-		if (!j) {
-			nlfsr(((uint32_t*)&key), ((uint32_t*)&key)+1, 0x54d4d555u, parity);
+
+		if(!j) {
+			nlfsr(((uint32_t*)&key), ((uint32_t*)&key) + 1, 0x54d4d555u, parity);
 		}
+
 		c = ((uint8_t*)&key)[j];
 
 		buffer[i] = c ^ data[i];
--- src/stream.c
+++ src/stream.c
@@ -8,17 +8,18 @@
 int main() {
 	//FILE* fp = fopen("/tmp/test.txt", "w+");
 
-	const char * string = "This is an example string";
+	const char* string = "This is an example string";
 
 	puts(string);
 	puts("\n");
 
 	uint8_t buffer[26];
-	scmo_encrypt((uint8_t*)string, buffer, strlen(string)+1, 0xdeadbeefcafebabe);
+	scmo_encrypt((uint8_t*)string, buffer, strlen(string) + 1, 0xdeadbeefcafebabe);
 
 	for(uint8_t i = 0; i < 26; i++) {
 		printf("%02X", buffer[i]);
 	}
+
 	puts("\n");
 
 }
