ad3edf3b9a5f5e212060f8d258468e74c5072096
Sebastian Green-Husted <ricecake@tfm.nu>
2016-10-14 19:27:29 -0400

    start of making the ciper self sync

--- src/scmo.c
+++ src/scmo.c
@@ -1,10 +1,14 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
 
 #include "scmo.h"
 #include "util.h"
 
 uint8_t* scmo_cipher(uint8_t* data, uint8_t* buffer, size_t data_size, scmo_key key) {
+	uint8_t previous[8];
+	memcpy((void*)previous, (void*)(key+2), 8);
+	//= (uint8_t*)(key+2);
 	for(size_t i = 0; i < data_size; i++) {
 		uint8_t j = i % 4;
 		uint8_t c;
@@ -15,7 +19,8 @@ uint8_t* scmo_cipher(uint8_t* data, uint8_t* buffer, size_t data_size, scmo_key 
 
 		c = ((uint8_t*)key)[j];
 
-		buffer[i] = c ^ data[i];
+		buffer[i] = previous[(i+1)%8] ^ (c ^ data[i]);
+		previous[i%8] = buffer[i];
 	}
 
 	return buffer;
