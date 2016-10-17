50c66b23a37a695a0d244f7dcfb0744f0e923b0c
Sebastian Green-Husted <ricecake@tfm.nu>
2016-10-16 21:03:01 -0400

    Update scmo.h

--- include/scmo.h
+++ include/scmo.h
@@ -7,6 +7,11 @@
 //typedef uint64_t scmo_key;
 typedef uint32_t* scmo_key;
 
+typedef struct {
+    scmo_key key,
+    uint32_t state
+} scmo_state;
+
 uint8_t* scmo_cipher(uint8_t*, uint8_t*, size_t, scmo_key);
 uint8_t* scmo_decrypt(uint8_t*, uint8_t*, size_t, scmo_key);
 
